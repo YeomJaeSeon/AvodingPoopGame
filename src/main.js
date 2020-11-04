4a45824084421e44b5a42dfea101251e9dc0a16c
YeomJaeSeon <a89541457@gmail.com>
2020-11-04 23:42:46 +0900

    버튼 오른쪽 왼쪾 눌렀을때 .. 움직임은 구현아직

--- src/main.js
+++ src/main.js
@@ -3,8 +3,22 @@
 const gameBtn = document.querySelector(".game__button");
 const field = document.querySelector(".game__field");
 
+let human = undefined;
+
 let started = false;
 let poopTimer = undefined;
+
+//사람 이동함.~
+window.addEventListener("keydown", (event) => {
+  if (!started) return;
+  // const humanPosition = human.getBoundingClientRect().left;
+  console.log(human.style.left);
+  if (event.key === "ArrowRight") {
+    console.log("오른쪾이동");
+  } else if (event.key === "ArrowLeft") {
+    console.log("왼쪽이동");
+  }
+});
 
 gameBtn.addEventListener("click", () => {
   if (!started) {
@@ -29,9 +43,13 @@ function init() {
 function addHuman() {
   const img = document.createElement("img");
   img.setAttribute("src", "imgs/human.png");
+  img.setAttribute("class", "human");
   img.style.position = "absolute";
   img.style.bottom = "0px";
   field.appendChild(img);
+
+  human = document.querySelector(".human");
+  // console.log(human.style.left);
 }
 
 function rainPoop() {}
