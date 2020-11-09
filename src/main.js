725a3ef8a8dc26a82066f3ca1d443b4c606c30bd
YeomJaeSeon <a89541457@gmail.com>
2020-11-09 17:09:22 +0900

    똥 생성, 똥 위에서 display되서 아래로 떨어지는거 구현, 여러개의 똥을 아직 구현못함

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 "use strict";
 
+const POOPSIZE = 50;
+
+const game = document.querySelector(".game");
+
 const gameBtn = document.querySelector(".game__button");
 const field = document.querySelector(".game__field");
 const fieldRect = field.getBoundingClientRect();
 
 let human = undefined;
+let poop = undefined;
 
 let started = false;
 let poopTimer = undefined;
@@ -37,13 +42,24 @@ gameBtn.addEventListener("click", () => {
 function startGame() {
   started = true;
   init();
+  changeGameBtn();
 }
 function stopGame() {
   started = false;
+  hideGameBtn();
 }
 
 function init() {
   addHuman();
+  rainPoop();
+}
+function changeGameBtn() {
+  const gameBtn = document.querySelector(".fas");
+  gameBtn.classList.remove("fa-play");
+  gameBtn.classList.add("fa-stop");
+}
+function hideGameBtn() {
+  gameBtn.style.visibility = "hidden";
 }
 
 function addHuman() {
@@ -53,7 +69,34 @@ function addHuman() {
   img.style.position = "absolute";
   img.style.bottom = "0px";
   field.appendChild(img);
+  img.style.left = "225px";
   human = document.querySelector(".human");
 }
 
-function rainPoop() {}
+function addPoop() {
+  const img = document.createElement("img");
+  img.setAttribute("src", "imgs/poop.png");
+  img.setAttribute("class", "poop");
+  img.style.position = "absolute";
+  img.style.left = `${getRandom(0, fieldRect.width - POOPSIZE)}px`;
+  img.style.top = "0px";
+  game.appendChild(img);
+  poop = document.querySelector(".poop");
+}
+
+function rainPoop() {
+  let poopPosition = 0;
+  addPoop();
+  poopTimer = setInterval(() => {
+    if (poop.getBoundingClientRect().bottom > fieldRect.bottom + 20) {
+      return;
+    }
+    console.log("똥비내려");
+    poopPosition += 50;
+    poop.style.transform = `translateY(${poopPosition}px)`;
+  }, 1000);
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
