f7acdcf50b54e5e011eb0b179fcb274eaaa0f6cc
YeomJaeSeon <a89541457@gmail.com>
2020-11-11 13:59:33 +0900

    사람이 똥맞으면 게임 멈춤 구현+ 전체적인 게임 타이머 구현

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 "use strict";
 
-const POOPSIZE = 50;
+const STOP_REASON = Object.freeze({
+  stop: "stop",
+  lose: "lose",
+});
+
+const POOPSIZE = 40;
+const HUMANSIZE = 60;
+const GAME_DURATION = 30;
 
 const game = document.querySelector(".game");
 
 const gameBtn = document.querySelector(".game__button");
+const gameTimer = document.querySelector(".game__timer");
+
 const field = document.querySelector(".game__field");
 const fieldRect = field.getBoundingClientRect();
 
@@ -12,9 +21,8 @@ let human = undefined;
 // let poop = undefined;
 
 let started = false;
-let poopTimer = ["poop"]; // 모든똥에대한 타이머를가지고있는
-//배열
-
+let timer = undefined;
+let poopTimer = ["poop"]; // 모든똥에대한 타이머를가지고있는 배열
 let poopTotalTimer = undefined;
 
 let humanPosition = 0; // 사람 현재 위치
@@ -38,7 +46,7 @@ gameBtn.addEventListener("click", () => {
   if (!started) {
     startGame();
   } else {
-    stopGame();
+    stopGame(STOP_REASON.stop);
   }
 });
 
@@ -46,18 +54,47 @@ function startGame() {
   started = true;
   init();
   changeGameBtn();
+  startGameTimer();
 }
-function stopGame() {
+//게임 사용자가 멈출경우, 게임이 져서 멈출경우 두경우 존재
+function stopGame(reason) {
   started = false;
   hideGameBtn();
   stopPoop();
+  stopGameTimer();
+  switch (reason) {
+    case STOP_REASON.stop:
+      console.log("stop!?!?");
+      break;
+    case STOP_REASON.lose:
+      console.log("loseㅠ.ㅠ");
+      break;
+  }
 }
 
 function init() {
   addHuman();
   displayTotalPoop();
 }
-
+function startGameTimer() {
+  let remainTimes = GAME_DURATION;
+  updateGameTimer(remainTimes);
+  timer = setInterval(() => {
+    updateGameTimer(--remainTimes);
+    if (remainTimes <= 0) {
+      stopGameTimer();
+    }
+  }, 1000);
+}
+function stopGameTimer() {
+  clearInterval(timer);
+}
+function updateGameTimer(time) {
+  console.log(gameTimer);
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  gameTimer.innerText = `${minutes}:${seconds}`;
+}
 function changeGameBtn() {
   const gameBtn = document.querySelector(".fas");
   gameBtn.classList.remove("fa-play");
@@ -90,15 +127,28 @@ function addPoop() {
   return document.querySelectorAll(".poop");
 }
 
-function movePoop(poop, time, poopTimer, i) {
+function movePoop(poop, poopTimer, i) {
   let poopPosition = 0;
   poopTimer[i] = setInterval(() => {
-    if (poop[i].getBoundingClientRect().bottom > fieldRect.bottom + POOPSIZE) {
+    if (poop[i].getBoundingClientRect().bottom > fieldRect.bottom + 150) {
       clearInterval(poopTimer);
     }
     poopPosition += 20;
     poop[i].style.transform = `translateY(${poopPosition}px)`;
-  }, time);
+
+    // 똥과 사람이 부딪힘 -> 게임질경우
+    if (
+      poop[i].getBoundingClientRect().bottom - 8 >=
+        human.getBoundingClientRect().top &&
+      fieldRect.bottom > poop[i].getBoundingClientRect().bottom &&
+      poop[i].getBoundingClientRect().right - 35 >
+        human.getBoundingClientRect().left &&
+      poop[i].getBoundingClientRect().left + 35 <
+        human.getBoundingClientRect().right
+    ) {
+      stopGame(STOP_REASON.lose);
+    }
+  }, getRandom(100, 200));
 }
 
 function stopPoop() {
@@ -110,7 +160,7 @@ function stopPoop() {
 
 //하나의 타이머와 하나의 똥에관한 함수
 function displayOnePoop(poopTimer, poop, i) {
-  movePoop(poop, getRandom(100, 1000), poopTimer, i);
+  movePoop(poop, poopTimer, i);
 }
 
 function displayTotalPoop() {
@@ -120,7 +170,7 @@ function displayTotalPoop() {
     //똥 timer배열자체와 똥 element배열자체와 식별자 i를전달
     poopTimer.push("poop");
     ++i;
-  }, getRandom(100, 2000));
+  }, getRandom(100, 1000));
 }
 
 function getRandom(min, max) {
